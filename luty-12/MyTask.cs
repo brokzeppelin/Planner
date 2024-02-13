157fc08d66d02f2f356c93c6daa02a67abe46bf1
brokzeppelin <brokzeppelin@ELIRENIO>
2024-02-13 02:57:51 +0100

    Add project files.

---
+++ luty-12/MyTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planner
+{
+    internal class MyTask
+    {
+        private string _name;
+        private double _duration;
+        private TimeSpan _progress;
+
+        public MyTask(string name, double duration, TimeSpan progress)
+        {
+            _name = name;
+            _duration = duration;
+            _progress = progress;
+        }
+
+        override public String ToString()
+        {
+            return _name;
+        }
+    }
+}
